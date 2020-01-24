7c4cd384b30b5b3550bed7b28e44e3442e75ab89
SleepingTea98 <chrisweston98@gmail.com>
2020-01-24 13:11:47 +0000

    Imported more blocks
    -

--- src/main/java/net/kineticdevelopment/arcana/common/init/BlockInit.java
+++ src/main/java/net/kineticdevelopment/arcana/common/init/BlockInit.java
@@ -29,7 +29,19 @@ public class BlockInit {
 	public static final Block TAINTED_ROCK = new TaintedBlockBase("tainted_rock", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
 	public static final Block TAINTED_SOIL = new TaintedBlockBase("tainted_soil", Material.GROUND).setCreativeTab(Main.TAB_TAINTARCANA);
 	public static final Block TAINTED_GRASS = new TaintedBlockBase("tainted_grass_block", Material.GRASS).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_ACACIA_PLANKS = new TaintedBlockBase("tainted_acacia_planks", Material.GRASS).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_AMBER_ORE = new TaintedBlockBase("tainted_amber_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_ARCANIUM_BLOCK = new TaintedBlockBase("tainted_arcanium_block", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_ARCANIUM_ORE = new TaintedBlockBase("tainted_arcanium_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
 
+	//Tainted Logs
+	public static final Block STRIPPED_TAINTED_GREATWOOD_LOG = new TaintedBlockBase("stripped_tainted_greatwood_log", Material.WOOD).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block STRIPPED_TAINTED_HAWTHORN_LOG = new TaintedBlockBase("stripped_tainted_hawthorn_log", Material.WOOD).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block STRIPPED_TAINTED_WILLOW_LOG = new TaintedBlockBase("stripped_tainted_willow_log", Material.WOOD).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_ACACIA_LOG = new TaintedBlockBase("tainted_acacia_log", Material.WOOD).setCreativeTab(Main.TAB_TAINTARCANA);
+
+	//Tainted Stairs
+	public static final Block TAINTED_ACACIA_STAIRS = new StairsBase("tainted_acacia_stairs", TAINTED_ACACIA_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_TAINTARCANA);
 
 	//Metal Based Blocks
 	public static final Block THAUMIUM_BLOCK = new BlockBase("thaumium_block", Material.IRON).setCreativeTab(Main.TAB_ARCANA);
@@ -39,15 +51,36 @@ public class BlockInit {
 	public static final Block ARCANE_STONE = new BlockBase("arcane_stone", Material.ROCK).setCreativeTab(Main.TAB_ARCANA);
 	public static final Block AMBER_ORE = new BlockBase("amber_ore", Material.ROCK).setCreativeTab(Main.TAB_ARCANA);
 	public static final Block ARCANE_STONE_BRICKS = new BlockBase("arcane_stone_bricks", Material.ROCK).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block INFUSION_ARCANE_STONE = new BlockBase("infusion_arcane_stone", Material.ROCK).setCreativeTab(Main.TAB_ARCANA);
+
+	//Grass
+	public static final Block MAGICAL_GRASS = new BlockBase("magical_grass", Material.GRASS).setCreativeTab(Main.TAB_ARCANA);
 
 	//Logs
 	public static final Block DAIR_LOG = new LogsBase("dair_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block DEAD_LOG = new LogsBase("dead_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block GREATWOOD_LOG = new LogsBase("greatwood_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block HAWTHORN_LOG = new LogsBase("hawthorn_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block STRIPPED_DAIR_LOG = new LogsBase("stripped_dair_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block STRIPPED_GREATWOOD_LOG = new LogsBase("stripped_greatwood_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block STRIPPED_HAWTHORN_LOG = new LogsBase("stripped_hawthorn_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block STRIPPED_WILLOW_LOG = new LogsBase("stripped_willow_log", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+
 
 	//Wood
 	public static final Block DAIR_PLANKS = new PlanksBase("dair_planks", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block DEAD_PLANKS = new PlanksBase("dead_planks", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block GREATWOOD_PLANKS = new PlanksBase("greatwood_planks", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block HAWTHORN_PLANKS = new PlanksBase("hawthorn_planks", Material.WOOD).setCreativeTab(Main.TAB_ARCANA);
+
 	//Stairs
 	public static final Block ARCANE_STONE_STAIRS = new StairsBase("arcane_stone_stairs", ARCANE_STONE.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
 	public static final Block ARCANE_STONE_BRICKS_STAIRS = new StairsBase("arcane_stone_bricks_stairs", ARCANE_STONE_BRICKS.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block DAIR_STAIRS = new StairsBase("dair_stairs", DAIR_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block DEAD_STAIRS = new StairsBase("dead_stairs", DEAD_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block GREATWOOD_STAIRS = new StairsBase("greatwood_stairs", GREATWOOD_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block HAWTHORN_STAIRS = new StairsBase("hawthorn_stairs", HAWTHORN_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_ARCANA);
+
 
 
 
---
+++ src/main/java/net/kineticdevelopment/arcana/common/objects/blocks/TaintedLogBlock.java
@@ -0,0 +1,128 @@
+package net.kineticdevelopment.arcana.common.objects.blocks;
+
+import net.kineticdevelopment.arcana.common.init.BlockStateInit;
+import net.kineticdevelopment.arcana.core.Main;
+import net.kineticdevelopment.arcana.utilities.IHasModel;
+import net.kineticdevelopment.arcana.utilities.taint.TaintHandler;
+import net.kineticdevelopment.arcana.utilities.taint.TaintLevelHandler;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockAir;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TaintedLogBlock extends LogsBase implements IHasModel {
+    public static final PropertyBool FULLYTAINTED = BlockStateInit.FULLYTAINTED;
+
+    public TaintedLogBlock(String name, Material material) {
+        super(name, material);
+        this.setDefaultState(this.getDefaultState().withProperty(BlockStateInit.FULLYTAINTED, false));
+        this.setTickRandomly(true);
+    }
+
+    @Override
+    public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random random) {
+        int h = 0;
+
+        int f = (int) TaintLevelHandler.getTaintLevel(worldIn);
+
+        if (f >= 5 && f <= 9) {
+            h = ThreadLocalRandom.current().nextInt(0, 10);
+        } else if (f >= 10 && f <= 19) {
+            h = ThreadLocalRandom.current().nextInt(0, 9);
+        } else if (f >= 20 && f <= 29) {
+            h = ThreadLocalRandom.current().nextInt(0, 8);
+        } else if (f >= 30 && f <= 39) {
+            h = ThreadLocalRandom.current().nextInt(0, 7);
+        } else if (f >= 40 && f <= 49) {
+            h = ThreadLocalRandom.current().nextInt(0, 6);
+        } else if (f >= 50 && f <= 59) {
+            h = ThreadLocalRandom.current().nextInt(0, 5);
+        } else if (f >= 60 && f <= 69) {
+            h = ThreadLocalRandom.current().nextInt(0, 4);
+        } else if (f >= 70 && f <= 79) {
+            h = ThreadLocalRandom.current().nextInt(0, 3);
+        } else if (f >= 80 && f <= 89) {
+            h = ThreadLocalRandom.current().nextInt(0, 2);
+        } else if (f >= 90 && f <= 99) {
+            h = ThreadLocalRandom.current().nextInt(0, 1);
+        } else if (f >= 100) {
+            h = 1;
+        }
+        if (h == 1) {
+            TaintHandler.spreadTaint(worldIn, pos);
+        }
+
+        boolean surrounded = true;
+
+        for (int x = -1; x < 2; x++) {
+
+            for (int y = -1; y < 2; y++) {
+
+                for (int z = -1; z < 2; z++) {
+
+                    BlockPos nPos = pos.add(x, y, z);
+
+                    Block b = worldIn.getBlockState(nPos).getBlock();
+
+                    if(!(b instanceof TaintedBlockBase) && !(b instanceof BlockAir)) {
+                        surrounded = false;
+                        System.out.println("Yeet");
+                    }
+                }
+            }
+        }
+
+        if(surrounded == true) {
+
+            worldIn.setBlockState(pos, state.withProperty(BlockStateInit.FULLYTAINTED, true));
+            this.setTickRandomly(false);
+            System.out.println("Yote");
+
+        }
+    }
+
+    public int getMetaFromState(IBlockState state) {
+        int i = 0;
+        boolean tainted = state.getValue(FULLYTAINTED);
+
+        if(tainted) {
+            i = 1;
+        } else {
+            i = 0;
+        }
+
+        return i;
+    }
+
+    public IBlockState getStateFromMeta(int meta) {
+        boolean tainted = false;
+        int i = meta;
+
+        if(i == 1) {
+            tainted = true;
+        } else if(i == 0) {
+            tainted = false;
+        }
+
+        return this.getDefaultState().withProperty(FULLYTAINTED, tainted);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {FULLYTAINTED});
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+}
