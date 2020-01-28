4af032c7b427a78010530bea39a5b83e31390791
Mozaran <apal2807@gmail.com>
2020-01-27 21:46:50 -0500

    Fixed blockstate json formatting issues and added a few tainted ores/blocks

--- src/main/java/net/kineticdevelopment/arcana/common/init/BlockInit.java
+++ src/main/java/net/kineticdevelopment/arcana/common/init/BlockInit.java
@@ -20,15 +20,26 @@ public class BlockInit {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	//Tainted
-	public static final Block TAINTED_CRUST = new TaintedBlockBase("tainted_crust", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA).setTickRandomly(true);
-	public static final Block TAINTED_GRAVEL = new TaintedBlockBase("tainted_gravel", Material.GROUND).setCreativeTab(Main.TAB_TAINTARCANA);
-	public static final Block TAINTED_ROCK = new TaintedBlockBase("tainted_rock", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
-	public static final Block TAINTED_SOIL = new TaintedBlockBase("tainted_soil", Material.GROUND).setCreativeTab(Main.TAB_TAINTARCANA);
-	public static final Block TAINTED_GRASS = new TaintedBlockBase("tainted_grass_block", Material.GRASS).setCreativeTab(Main.TAB_TAINTARCANA);
 	public static final Block TAINTED_ACACIA_PLANKS = new TaintedBlockBase("tainted_acacia_planks", Material.GRASS).setCreativeTab(Main.TAB_TAINTARCANA);
 	public static final Block TAINTED_AMBER_ORE = new TaintedBlockBase("tainted_amber_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
-	public static final Block TAINTED_ARCANIUM_BLOCK = new TaintedBlockBase("tainted_arcanium_block", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
 	public static final Block TAINTED_ARCANIUM_ORE = new TaintedBlockBase("tainted_arcanium_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_CINNABAR_ORE = new TaintedBlockBase("tainted_cinnabar_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_CRUST = new TaintedBlockBase("tainted_crust", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA).setTickRandomly(true);
+	public static final Block TAINTED_DESTROYED_ORE = new TaintedBlockBase("tainted_destroyed_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_DIAMOND_ORE = new TaintedBlockBase("tainted_diamond_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_EMERALD_ORE = new TaintedBlockBase("tainted_emerald_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_GOLD_ORE = new TaintedBlockBase("tainted_gold_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_GRASS = new TaintedBlockBase("tainted_grass_block", Material.GRASS).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_GRAVEL = new TaintedBlockBase("tainted_gravel", Material.GROUND).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_IRON_ORE = new TaintedBlockBase("tainted_iron_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_LAPIS_ORE = new TaintedBlockBase("tainted_lapis_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_REDSTONE_ORE = new TaintedBlockBase("tainted_redstone_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_ROCK = new TaintedBlockBase("tainted_rock", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_RUBY_ORE = new TaintedBlockBase("tainted_ruby_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_SAND = new TaintedBlockBase("tainted_sand", Material.SAND).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_SILVER_ORE = new TaintedBlockBase("tainted_silver_ore", Material.ROCK).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_SOIL = new TaintedBlockBase("tainted_soil", Material.GROUND).setCreativeTab(Main.TAB_TAINTARCANA);
+
 
 	//Tainted Logs
 	public static final Block STRIPPED_TAINTED_GREATWOOD_LOG = new TaintedLogBlock("stripped_tainted_greatwood_log", Material.WOOD).setCreativeTab(Main.TAB_TAINTARCANA);
@@ -40,8 +51,17 @@ public class BlockInit {
 	public static final Block TAINTED_ACACIA_STAIRS = new StairsBase("tainted_acacia_stairs", TAINTED_ACACIA_PLANKS.getDefaultState()).setCreativeTab(Main.TAB_TAINTARCANA);
 
 	//Metal Based Blocks
-	public static final Block THAUMIUM_BLOCK = new BlockBase("thaumium_block", Material.IRON).setCreativeTab(Main.TAB_ARCANA);
 	public static final Block ARCANIUM_BLOCK = new BlockBase("arcanium_block", Material.IRON).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block THAUMIUM_BLOCK = new BlockBase("thaumium_block", Material.IRON).setCreativeTab(Main.TAB_ARCANA);
+	public static final Block TAINTED_ARCANIUM_BLOCK = new TaintedBlockBase("tainted_arcanium_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_COAL_BLOCK = new TaintedBlockBase("tainted_coal_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_DIAMOND_BLOCK = new TaintedBlockBase("tainted_diamond_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_EMERALD_BLOCK = new TaintedBlockBase("tainted_emerald_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_GOLD_BLOCK = new TaintedBlockBase("tainted_gold_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_IRON_BLOCK = new TaintedBlockBase("tainted_iron_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_LAPIS_BLOCK = new TaintedBlockBase("tainted_lapis_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_REDSTONE_BLOCK = new TaintedBlockBase("tainted_redstone_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
+	public static final Block TAINTED_THAUMIUM_BLOCK = new TaintedBlockBase("tainted_thaumium_block", Material.IRON).setCreativeTab(Main.TAB_TAINTARCANA);
 
 	//Stone Based Blocks
 	public static final Block ARCANE_STONE = new BlockBase("arcane_stone", Material.ROCK).setCreativeTab(Main.TAB_ARCANA);
