95119ea4b530c0e690e4424c4116b2818139d6f2
Luna <izzeldeen03@gmail.com>
2020-05-05 14:34:05 +0100

    Meta

--- src/main/java/net/arcanamod/Arcana.java
+++ src/main/java/net/arcanamod/Arcana.java
@@ -25,6 +25,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -45,10 +46,9 @@ import javax.annotation.Nonnull;
  *
  * @author Atlas
  */
-@Mod(modid = Arcana.MODID, name = Arcana.NAME, version = Arcana.VERSION)
+@Mod(Arcana.MODID)
 public class Arcana{
 	public static final String MODID = "arcana";
-	public static final String NAME = "Arcana";
 	public static final String VERSION = "1.0";
 	
 	public static final Logger logger = LogManager.getLogger("Arcana");
@@ -56,8 +56,8 @@ public class Arcana{
 	@Mod.Instance
 	public static Arcana instance;
 	
-	@SidedProxy(clientSide = "net.arcanamod.ClientProxy", serverSide = "net.arcanamod.CommonProxy")
-	public static CommonProxy proxy;
+	//@SidedProxy(clientSide = "net.arcanamod.ClientProxy", serverSide = "net.arcanamod.CommonProxy")
+	public static CommonProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> CommonProxy::new);
 	
 	/**
 	 * Preintialization Event
