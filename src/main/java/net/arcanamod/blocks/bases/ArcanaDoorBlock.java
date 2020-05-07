24a34717f5e779e5de1fea73110983145778843d
Luna <izzeldeen03@gmail.com>
2020-05-07 22:43:45 +0100

    Simplify saplings and trapdoors

---
+++ src/main/java/net/arcanamod/blocks/bases/ArcanaDoorBlock.java
@@ -0,0 +1,11 @@
+package net.arcanamod.blocks.bases;
+
+import net.minecraft.block.DoorBlock;
+
+// literally the same BS as ArcanaTrapDoorBlock
+public class ArcanaDoorBlock extends DoorBlock{
+	
+	public ArcanaDoorBlock(Properties properties){
+		super(properties);
+	}
+}
