5e2291d46b254edd141ee2f9c668dd1aea826a4e
DcZipPL <dczip89@gmail.com>
2020-05-09 23:36:06 +0200

    Fixed phials names

--- src/main/java/net/arcanamod/items/PhialItem.java
+++ src/main/java/net/arcanamod/items/PhialItem.java
@@ -5,6 +5,7 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -29,5 +30,6 @@ public class PhialItem extends Item
 
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn){
         tooltip.add(new TranslationTextComponent("aspect." + aspectName + ".desc"));
+        tooltip.add(new StringTextComponent("x8"));
     }
 }
