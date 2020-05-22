e8db1458a9b2069f5243ae2bdd3171238352dc5a
DcZipPL <dczip89@gmail.com>
2020-05-22 20:33:30 +0200

    Updated getAspectByName

--- src/main/java/net/arcanamod/aspects/Aspects.java
+++ src/main/java/net/arcanamod/aspects/Aspects.java
@@ -61,122 +61,14 @@ public class Aspects{
 	public static Aspect getAspectByName(@Nullable String name){
 		if(name == null)
 			return null;
-		switch(name.toUpperCase()){
-			default:
-				return null;
-			case "AIR":
-				return Aspect.AIR;
-			case "ARMOR":
-				return Aspect.ARMOUR;
-			case "AURA":
-				return Aspect.AURA;
-			case "BEAST":
-				return Aspect.BEAST;
-			case "CHAOS":
-				return Aspect.CHAOS;
-			case "CRAFTING":
-				return Aspect.CRAFTING;
-			case "CRYSTAL":
-				return Aspect.CRYSTAL;
-			case "DARKNESS":
-				return Aspect.DARKNESS;
-			case "DEATH":
-				return Aspect.DEATH;
-			case "EARTH":
-				return Aspect.EARTH;
-			case "ELDRITCH":
-				return Aspect.ELDRITCH;
-			case "ENDER":
-				return Aspect.ENDER;
-			case "ENVY":
-				return Aspect.ENVY;
-			case "EXCHANGE":
-				return Aspect.EXCHANGE;
-			case "FABRIC":
-				return Aspect.FABRIC;
-			case "FIRE":
-				return Aspect.FIRE;
-			case "FLESH":
-				return Aspect.FLESH;
-			case "FLIGHT":
-				return Aspect.FLIGHT;
-			case "GLUTTONY":
-				return Aspect.GLUTTONY;
-			case "GREED":
-				return Aspect.GREED;
-			case "HARVEST":
-				return Aspect.HARVEST;
-			case "HUMAN":
-				return Aspect.HUMAN;
-			case "ICE":
-				return Aspect.ICE;
-			case "IMPRISON":
-				return Aspect.IMPRISON;
-			case "JOURNEY":
-				return Aspect.JOURNEY;
-			case "LIFE":
-				return Aspect.LIFE;
-			case "LIGHT":
-				return Aspect.LIGHT;
-			case "LUST":
-				return Aspect.LUST;
-			case "MACHINE":
-				return Aspect.MACHINE;
-			case "MANA":
-				return Aspect.MANA;
-			case "METAL":
-				return Aspect.METAL;
-			case "MINING":
-				return Aspect.MINING;
-			case "MOVEMENT":
-				return Aspect.MOVEMENT;
-			case "NETHER":
-				return Aspect.NETHER;
-			case "ORDER":
-				return Aspect.ORDER;
-			case "PLANT":
-				return Aspect.PLANT;
-			case "PRIDE":
-				return Aspect.PRIDE;
-			case "SEEDS":
-				return Aspect.SEEDS;
-			case "SENSES":
-				return Aspect.SENSES;
-			case "SLIME":
-				return Aspect.SLIME;
-			case "SLOTH":
-				return Aspect.SLOTH;
-			case "SPIRIT":
-				return Aspect.SPIRIT;
-			case "STRENGTH":
-				return Aspect.STRENGTH;
-			case "TAINT":
-				return Aspect.TAINT;
-			case "TOOL":
-				return Aspect.TOOL;
-			case "TREE":
-				return Aspect.TREE;
-			case "UNDEAD":
-				return Aspect.UNDEAD;
-			case "VACUUM":
-				return Aspect.VACUUM;
-			case "VENOM":
-				return Aspect.VENOM;
-			case "WATER":
-				return Aspect.WATER;
-			case "WEAPON":
-				return Aspect.WEAPON;
-			case "WIND":
-				return Aspect.WIND;
-			case "WRATH":
-				return Aspect.WRATH;
-			case "OVERWORLD":
-				return Aspect.OVERWORLD;
-			case "ENERGY":
-				return Aspect.ENERGY;
-			case "MIND":
-				return Aspect.MIND;
+		for (Aspect aspect : Aspect.values())
+		{
+			if (aspect.name().toLowerCase().equals(name.toLowerCase()))
+			{
+				return aspect;
+			}
 		}
+		return null;
 	}
 
 	public static boolean areAspectsConnected(Aspect a, Aspect b){
