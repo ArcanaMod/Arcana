2c83461228cb424678859ef433c5a25138d85143
Luna <izzeldeen03@gmail.com>
2020-05-23 21:41:36 +0100

    Add wands to creative tab.

--- src/main/java/net/arcanamod/items/WandItem.java
+++ src/main/java/net/arcanamod/items/WandItem.java
@@ -3,14 +3,16 @@ package net.arcanamod.items;
 import mcp.MethodsReturnNonnullByDefault;
 import net.arcanamod.items.attachment.*;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-// DynamicBucketModel
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class WandItem extends Item{
@@ -36,5 +38,25 @@ public class WandItem extends Item{
 	
 	public ITextComponent getDisplayName(ItemStack stack){
 		return new TranslationTextComponent(getCore(stack).getCoreTranslationKey(), new TranslationTextComponent(getCap(stack).getPrefixTranslationKey()));
+	}
+	
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items){
+		if(isInGroup(group)){
+			// iron/wooden, silver/dair, gold/greatwood, thaumium/silverwood, void/arcanium
+			items.add(withCapAndCore("iron_cap", "wood_wand"));
+			items.add(withCapAndCore("silver_cap", "dair_wand"));
+			items.add(withCapAndCore("gold_cap", "greatwood_wand"));
+			items.add(withCapAndCore("void_cap", "arcanium_wand"));
+		}
+	}
+	
+	// change for recipes
+	private ItemStack withCapAndCore(String cap, String core){
+		CompoundNBT nbt = new CompoundNBT();
+		nbt.putString("cap", "arcana:" + cap);
+		nbt.putString("core", "arcana:" + core);
+		ItemStack stack = new ItemStack(this, 1);
+		stack.setTag(nbt);
+		return stack;
 	}
 }
