c9b3f5180705a172d5597fb832e38f0db2a5cd0d
Falkor220 <63330954+Falkor220@users.noreply.github.com>
2020-06-10 20:00:09 +0100

    Scribbled notes now are only given when the player first joins and now have a recipe, still untextured

--- src/main/java/net/arcanamod/event/LoggedInEvent.java
+++ src/main/java/net/arcanamod/event/LoggedInEvent.java
@@ -3,8 +3,12 @@ package net.arcanamod.event;
 import net.arcanamod.ArcanaConfig;
 import net.arcanamod.items.ArcanaItems;
 import net.minecraft.advancements.AdvancementManager;
-import net.minecraft.advancements.PlayerAdvancements;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,11 +21,13 @@ public class LoggedInEvent{
     //gives a player the scribbled notes item
     @SubscribeEvent
     public static void onJoin(PlayerEvent.PlayerLoggedInEvent event) {
-        /*
         if (ArcanaConfig.SPAWN_WITH_NOTES.get()) {
-            event.getPlayer().inventory.addItemStackToInventory(new ItemStack(ArcanaItems.SCRIBBLED_NOTES.get()));
+            PlayerEntity player =event.getPlayer();
+            CompoundNBT data = player.getPersistentData();
+            if(!data.contains(PlayerEntity.PERSISTED_NBT_TAG)){
+                event.getPlayer().inventory.addItemStackToInventory(new ItemStack(ArcanaItems.SCRIBBLED_NOTES.get()));
+                data.put(PlayerEntity.PERSISTED_NBT_TAG, new CompoundNBT());
+            }
         }
-
-         */
     }
 }
