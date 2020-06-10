1467b89847f7a72a17b29dcb1a19cb3512517afb
Falkor220 <63330954+Falkor220@users.noreply.github.com>
2020-06-10 21:44:09 +0100

    Fixed Persistent tag for player

--- src/main/java/net/arcanamod/event/LoggedInEvent.java
+++ src/main/java/net/arcanamod/event/LoggedInEvent.java
@@ -18,15 +18,23 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class LoggedInEvent{
 
+    public static final String TAG_HAS_RECEIVED_NOTES = "arcana-HasReceivedNotes";
+
     //gives a player the scribbled notes item
     @SubscribeEvent
     public static void onJoin(PlayerEvent.PlayerLoggedInEvent event) {
         if (ArcanaConfig.SPAWN_WITH_NOTES.get()) {
             PlayerEntity player =event.getPlayer();
             CompoundNBT data = player.getPersistentData();
-            if(!data.contains(PlayerEntity.PERSISTED_NBT_TAG)){
-                event.getPlayer().inventory.addItemStackToInventory(new ItemStack(ArcanaItems.SCRIBBLED_NOTES.get()));
+            if(!data.contains(PlayerEntity.PERSISTED_NBT_TAG)) {
                 data.put(PlayerEntity.PERSISTED_NBT_TAG, new CompoundNBT());
+            }
+            CompoundNBT persistent = data.getCompound(PlayerEntity.PERSISTED_NBT_TAG);
+            boolean fail = false;
+
+            if(fail || !persistent.getBoolean(TAG_HAS_RECEIVED_NOTES)){
+                event.getPlayer().inventory.addItemStackToInventory(new ItemStack(ArcanaItems.SCRIBBLED_NOTES.get()));
+                persistent.putBoolean(TAG_HAS_RECEIVED_NOTES, true);
             }
         }
     }
