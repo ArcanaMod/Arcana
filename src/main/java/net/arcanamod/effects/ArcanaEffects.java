a68cbc920201baa4d21950b82c71441a74b81ed3
Falkor220 <63330954+Falkor220@users.noreply.github.com>
2020-07-02 16:10:19 +0100

    Added tainted effect

---
+++ src/main/java/net/arcanamod/effects/ArcanaEffects.java
@@ -0,0 +1,22 @@
+package net.arcanamod.effects;
+
+import net.arcanamod.Arcana;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ArcanaEffects extends Effect {
+
+    public ArcanaEffects(EffectType p_i50391_1_, int p_i50391_2_) {
+        super(p_i50391_1_, p_i50391_2_);
+    }
+
+
+    public static final DeferredRegister<Effect> EFFECTS = new DeferredRegister<>(ForgeRegistries.POTIONS, Arcana.MODID);
+
+    public static final RegistryObject<Effect> TAINTED = EFFECTS.register("tainted", () -> new TaintedEffect());
+
+
+}
