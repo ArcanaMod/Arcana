a33a819ba64f44cf11845d8cdc16310362a7345a
Luna <izzeldeen03@gmail.com>
2020-07-15 12:52:26 +0100

    Adjust aspect tooltip colours

--- src/main/java/net/arcanamod/client/event/RenderTooltip.java
+++ src/main/java/net/arcanamod/client/event/RenderTooltip.java
@@ -1,14 +1,16 @@
 package net.arcanamod.client.event;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.arcanamod.aspects.AspectStack;
 import net.arcanamod.aspects.AspectUtils;
 import net.arcanamod.aspects.ItemAspectRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -18,7 +20,6 @@ import net.minecraftforge.fml.common.Mod;
 import javax.annotation.Nonnull;
 import java.awt.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mod.EventBusSubscriber
 public class RenderTooltip{
@@ -49,7 +50,14 @@ public class RenderTooltip{
 				for(AspectStack stack : stacks){
 					ItemStack aspect = AspectUtils.getItemStackForAspect(stack.getAspect());
 					mc.getItemRenderer().renderItemAndEffectIntoGUI(aspect, x, y);
-					mc.getItemRenderer().renderItemOverlayIntoGUI(mc.fontRenderer, aspect, x, y, String.valueOf(stack.getAmount()));
+					// render text
+					MatrixStack matrixstack = new MatrixStack();
+					String s = String.valueOf(stack.getAmount());
+					matrixstack.translate(0, 0, mc.getItemRenderer().zLevel + 200.0F);
+					IRenderTypeBuffer.Impl impl = IRenderTypeBuffer.getImpl(Tessellator.getInstance().getBuffer());
+					mc.fontRenderer.renderString(s, x + 19 - mc.fontRenderer.getStringWidth(s), y + 10, blendToWhite(stack.getAspect().getColorRange().get()[3]), true, matrixstack.getLast().getMatrix(), impl, false, 0, 0xf000f0);
+					impl.finish();
+					
 					x += 20;
 				}
 				RenderSystem.popMatrix();
@@ -57,12 +65,30 @@ public class RenderTooltip{
 		}
 	}
 	
+	private static int blendToWhite(int a){
+		int aR = (a & 0xff0000) >> 16;
+		int aG = (a & 0xff00) >> 8;
+		int aB = a & 0xff;
+		int finR = (int)(aR * .5f + 127.5f);
+		int finG = (int)(aG * .5f + 127.5f);
+		int finB = (int)(aB * .5f + 127.5f);
+		return finR << 16 | finG << 8 | finB;
+	}
+	
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void makeTooltip(@Nonnull ItemTooltipEvent event){
 		if(Screen.hasShiftDown() && !ItemAspectRegistry.isProcessing()){
 			List<AspectStack> stacks = ItemAspectRegistry.get(event.getItemStack());
 			if(stacks.size() > 0){
-				String collect = stacks.stream().map(stack -> " ").collect(Collectors.joining());
+				// amount of spaces that need inserting
+				int filler = stacks.size() * 5;
+				// repeat " " *filler
+				StringBuilder sb = new StringBuilder();
+				for(int __ = 0; __ < filler; __++){
+					String s = " ";
+					sb.append(s);
+				}
+				String collect = sb.toString();
 				event.getToolTip().add(new StringTextComponent(collect));
 				event.getToolTip().add(new StringTextComponent(collect));
 			}
