9f9100658db3adceb642e91dab5627739c3e0aed
Luna <izzeldeen03@gmail.com>
2020-07-20 20:32:33 +0100

    Silver: destroys 99% of taint bugs.*

---
+++ src/main/java/net/arcanamod/items/tools/SilverAxeItem.java
@@ -0,0 +1,24 @@
+package net.arcanamod.items.tools;
+
+import mcp.MethodsReturnNonnullByDefault;
+import net.arcanamod.blocks.Taint;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class SilverAxeItem extends AxeItem{
+	
+	public SilverAxeItem(IItemTier tier, float attackDamage, float attackSpeed, Properties builder){
+		super(tier, attackDamage, attackSpeed, builder);
+	}
+	
+	public float getDestroySpeed(ItemStack stack, BlockState state){
+		float speed = super.getDestroySpeed(stack, state);
+		return Taint.isTainted(state.getBlock()) ? speed * 2 : speed;
+	}
+}
