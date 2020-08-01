085cd18487453eb43f3ea0653fbfbc7508ed4ee6
DcZipPL <dczip89@gmail.com>
2020-08-01 16:39:23 +0200

    Water gets all taint blocks around and change color.

--- src/main/java/net/arcanamod/mixin/BiomeColorMixin.java
+++ src/main/java/net/arcanamod/mixin/BiomeColorMixin.java
@@ -1,6 +1,8 @@
 package net.arcanamod.mixin;
 
 import net.arcanamod.blocks.ArcanaBlocks;
+import net.arcanamod.blocks.Taint;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ILightReader;
 import net.minecraft.world.biome.BiomeColors;
@@ -10,17 +12,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.awt.*;
+import java.util.Iterator;
 
 // Lets mixin into BiomeColors for taint "biome" water :)
 @Mixin(BiomeColors.class)
 public class BiomeColorMixin {
+	// TODO: I think that is better way to do this. Currently chunk loading is very slow
 	@Inject(method = "getWaterColor(Lnet/minecraft/world/ILightReader;Lnet/minecraft/util/math/BlockPos;)I", at = @At("HEAD"), remap = false, cancellable = true)
 	private static void getWaterColor(ILightReader worldIn, BlockPos blockPosIn, CallbackInfoReturnable<Integer> callbackInfoReturnable) {
-		BlockPos.getAllInBox(blockPosIn.add(3,3,3),blockPosIn.add(-3,-3,-3)).forEach(blockPos -> {
-			try{
-				if (worldIn.getBlockState(blockPos).getBlock() == ArcanaBlocks.TAINTED_SOIL.get())
-					callbackInfoReturnable.setReturnValue(Color.decode("#6e298e").getRGB());
-			}catch(ArrayIndexOutOfBoundsException ignore){}
-		});
+		if (worldIn.getBlockState(blockPosIn.up()).getBlock() != Blocks.WATER) {
+			Iterator<BlockPos> i = BlockPos.getAllInBox(blockPosIn.add(2, 3, 2), blockPosIn.add(-2, -3, -2)).iterator();
+			while (i.hasNext()) {
+				try {
+					if (Taint.getPureOfBlock(worldIn.getBlockState(i.next()).getBlock()) != null) {
+						callbackInfoReturnable.setReturnValue(0x6e298e);
+						break;
+					}
+				} catch (ArrayIndexOutOfBoundsException ignore) {
+				}
+			}
+		}
 	}
 }
