3a5a2bff04de2c5354521bc9b9c8e0f076d69ff9
DcZipPL <dczip89@gmail.com>
2020-08-04 17:15:16 +0200

    Searching water color issue

--- src/main/java/net/arcanamod/mixin/BiomeColorMixin.java
+++ src/main/java/net/arcanamod/mixin/BiomeColorMixin.java
@@ -1,8 +1,10 @@
 package net.arcanamod.mixin;
 
 import net.arcanamod.blocks.Taint;
+import net.arcanamod.client.gui.UiUtil;
 import net.arcanamod.util.MixinUtil;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ILightReader;
 import net.minecraft.world.biome.BiomeColors;
@@ -24,40 +26,43 @@ import static net.minecraft.world.biome.BiomeColors.WATER_COLOR;
 @OnlyIn(Dist.CLIENT)
 @Mixin(BiomeColors.class)
 public class BiomeColorMixin {
-	// TODO: I think that is better way to do this. Currently chunk loading is very slow
+	// TODO: I think that is better way to do this. Currently chunk loading is slower than normal
 	@Inject(method = "getWaterColor(Lnet/minecraft/world/ILightReader;Lnet/minecraft/util/math/BlockPos;)I", at = @At("HEAD"), remap = false, cancellable = true)
 	private static void getWaterColor(ILightReader worldIn, BlockPos blockPosIn, CallbackInfoReturnable<Integer> callbackInfoReturnable) {
-		if (worldIn.getBlockState(blockPosIn.up()).getBlock() != Blocks.WATER) {
-			Iterator<BlockPos> i0 = BlockPos.getAllInBoxMutable(blockPosIn.add(3, 3, 3), blockPosIn.add(-3, -3, -3)).iterator();
-			while (i0.hasNext()) {
-				try {
-					if (Taint.isTainted(worldIn.getBlockState(i0.next()).getBlock())) {
-						callbackInfoReturnable.setReturnValue(MixinUtil.blend(0x6e298e, MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR), 0.66f));
-						break;
+		try {
+			if (blockPosIn.getX()==-1&&blockPosIn.getZ()==71&&blockPosIn.getY()==62)
+				System.out.println("something");
+			int returnVal = MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR);
+			if (worldIn.getBlockState(blockPosIn.up()).getBlock() != Blocks.WATER) {
+				Iterator<BlockPos> i0 = BlockPos.getAllInBoxMutable(blockPosIn.add(3, 2, 3), blockPosIn.add(-3, -2, -3)).iterator();
+				while (i0.hasNext()) {
+					BlockPos b = i0.next();
+					if (Taint.isTainted(worldIn.getBlockState(b).getBlock())) {
+						if (worldIn.getBlockState(b).getBlock()!=Blocks.WATER) {
+							returnVal=UiUtil.blend(0x6e298e, MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR), 0.66f);
+							break;
+						}
 					}
-				} catch (ArrayIndexOutOfBoundsException ignore) {
 				}
-			}
-			Iterator<BlockPos> i1 = BlockPos.getAllInBoxMutable(blockPosIn.add(2, 3, 2), blockPosIn.add(-2, -3, -2)).iterator();
-			while (i1.hasNext()) {
-				try {
+				Iterator<BlockPos> i1 = BlockPos.getAllInBoxMutable(blockPosIn.add(2, 3, 2), blockPosIn.add(-2, -3, -2)).iterator();
+				while (i1.hasNext()) {
 					if (Taint.isTainted(worldIn.getBlockState(i1.next()).getBlock())) {
-						callbackInfoReturnable.setReturnValue(MixinUtil.blend(0x6e298e, MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR), 0.33f));
+						returnVal=UiUtil.blend(0x6e298e, MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR), 0.33f);
 						break;
 					}
-				} catch (ArrayIndexOutOfBoundsException ignore) {
 				}
-			}
-			Iterator<BlockPos> i = BlockPos.getAllInBoxMutable(blockPosIn.add(1, 3, 1), blockPosIn.add(-1, -3, -1)).iterator();
-			while (i.hasNext()) {
-				try {
-					if (Taint.isTainted(worldIn.getBlockState(i.next()).getBlock())) {
-						callbackInfoReturnable.setReturnValue(0x6e298e);
+				int i = 1;//for (int i = 0; i < Minecraft.getInstance().gameSettings.biomeBlendRadius; i++) {
+				Iterator<BlockPos> i2 = BlockPos.getAllInBoxMutable(blockPosIn.add(i, 3, i), blockPosIn.add(-i, -3, -i)).iterator();
+				while (i2.hasNext()) {
+					if (Taint.isTainted(worldIn.getBlockState(i2.next()).getBlock())) {
+						returnVal=UiUtil.blend(0x6e298e,MixinUtil.func_228359_a_(worldIn, blockPosIn, WATER_COLOR),/*Minecraft.getInstance().gameSettings.biomeBlendRadius/((float)100)*i)*/1f);
 						break;
 					}
-				} catch (ArrayIndexOutOfBoundsException ignore) {
 				}
-			}
+				//}
+			callbackInfoReturnable.setReturnValue(returnVal);
+		}
+		} catch (ArrayIndexOutOfBoundsException ignore) {
 		}
 	}
 }
--- src/main/java/net/arcanamod/util/MixinUtil.java
+++ src/main/java/net/arcanamod/util/MixinUtil.java
@@ -5,33 +5,7 @@ import net.minecraft.world.ILightReader;
 import net.minecraft.world.level.ColorResolver;
 
 public class MixinUtil {
-	public static int func_228359_a_(ILightReader worldIn, BlockPos blockPosIn, ColorResolver colorResolverIn){
+	public static int func_228359_a_(ILightReader worldIn, BlockPos blockPosIn, ColorResolver colorResolverIn) {
 		return worldIn.getBlockColor(blockPosIn, colorResolverIn);
-	}
-
-	public static int blend(int c1, int c2, float ratio) {
-		if ( ratio > 1f ) ratio = 1f;
-		else if ( ratio < 0f ) ratio = 0f;
-		float iRatio = 1.0f - ratio;
-
-		int i1 = c1;
-		int i2 = c2;
-
-		int a1 = (i1 >> 24 & 0xff);
-		int r1 = ((i1 & 0xff0000) >> 16);
-		int g1 = ((i1 & 0xff00) >> 8);
-		int b1 = (i1 & 0xff);
-
-		int a2 = (i2 >> 24 & 0xff);
-		int r2 = ((i2 & 0xff0000) >> 16);
-		int g2 = ((i2 & 0xff00) >> 8);
-		int b2 = (i2 & 0xff);
-
-		int a = (int)((a1 * iRatio) + (a2 * ratio));
-		int r = (int)((r1 * iRatio) + (r2 * ratio));
-		int g = (int)((g1 * iRatio) + (g2 * ratio));
-		int b = (int)((b1 * iRatio) + (b2 * ratio));
-
-		return a << 24 | r << 16 | g << 8 | b;
 	}
 }
