4328b71424aaa6b15b2c10eee8e77f2b73cee901
DcZipPL <dczip89@gmail.com>
2020-09-18 17:31:21 +0200

    Spell base.

---
+++ src/main/java/net/arcanamod/systems/spell/CastAspect.java
@@ -0,0 +1,7 @@
+package net.arcanamod.systems.spell;
+
+/**
+ * With CastAspect you can create Cast Combos
+ */
+public class CastAspect {
+}
--- src/main/java/net/arcanamod/systems/spell/ISpell.java
+++ src/main/java/net/arcanamod/systems/spell/ISpell.java
@@ -1,6 +1,14 @@
 package net.arcanamod.systems.spell;
 
+import net.arcanamod.aspects.Aspect;
+
 public interface ISpell {
+
+	Aspect getSpellAspect();
+	Aspect[] getModAspects(); // Mod 1, Mod 2
+	CastAspect[] getCastAspects(); // Cast, Cast+
+
+	int getComplexity();
 
 
 
