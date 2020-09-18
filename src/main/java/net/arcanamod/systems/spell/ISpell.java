3ffa0401789657e0d88e24dffdad8e97bc57b3c1
DcZipPL <dczip89@gmail.com>
2020-09-18 23:55:57 +0200

    More comments.

--- src/main/java/net/arcanamod/systems/spell/ISpell.java
+++ src/main/java/net/arcanamod/systems/spell/ISpell.java
@@ -9,18 +9,58 @@ import net.minecraft.entity.player.PlayerEntity;
  */
 public interface ISpell {
 
+	/**
+	 * Defines all variables. DON'T USE THAT IN REGISTRY!!!
+	 * @param modAspects Modifier Aspects
+	 * @param castAspects Cast Aspects
+	 * @param data extra data
+	 * @return this but with defined variables.
+	 */
 	ISpell build(Aspect[] modAspects, CastAspect[] castAspects, SpellExtraData data);
 
+	/**
+	 * Core aspect in spell.
+	 * @return returns core aspect.
+	 */
 	Aspect getSpellAspect();
-	Aspect[] getModAspects(); // Mod 1, Mod 2, Sin Mod
+
+	/**
+	 * Returns spell modifiers.
+	 * @return Mod 1, Mod 2, Sin Mod
+	 */
+	Aspect[] getModAspects();
+
+	/**
+	 * Return CastAspects that are "neat" modifiers and are used in combos.
+	 * "neat" is mod by Vazkii. Why I'm writing that in documentation???
+	 * @return returns CastAspect array.
+	 */
 	CastAspect[] getCastAspects(); // Cast, Cast+
 
+	/**
+	 * Cost of spell in AspectStacks.
+	 * @return returns cost of spell.
+	 */
 	AspectStack[] getAspectCosts();
 
+	/**
+	 * How spell is complex to use / create
+	 * @return returns spell complexity.
+	 */
 	int getComplexity();
 
+	// TODO: ADD COOLDOWN FUNCTION FOR "HOLD BUTTON" SPELLS
+
+	/**
+	 * Use of that spell.
+	 * @param player Player
+	 * @param action Click Action
+	 */
 	void use(PlayerEntity player, Action action);
 
+	/**
+	 * Click action
+	 */
 	enum Action{
 		USE,
 		ALT_USE,
