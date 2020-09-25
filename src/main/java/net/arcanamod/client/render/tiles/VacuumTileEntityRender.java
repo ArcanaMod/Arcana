a47e2a05ca69af8ced925dee39081e6805a468bb
DcZipPL <dczip89@gmail.com>
2020-09-25 11:23:41 +0200

    Vacuum TileEntityRender Zfighting fixed.

--- src/main/java/net/arcanamod/client/render/tiles/VacuumTileEntityRender.java
+++ src/main/java/net/arcanamod/client/render/tiles/VacuumTileEntityRender.java
@@ -46,22 +46,27 @@ public class VacuumTileEntityRender<T extends VacuumTileEntity> extends TileEnti
         float lvt_6_1_ = (RANDOM.nextFloat() * 0.5F + 0.1F) * p_228883_3_;
         float lvt_7_1_ = (RANDOM.nextFloat() * 0.5F + 0.4F) * p_228883_3_;
         float lvt_8_1_ = (RANDOM.nextFloat() * 0.5F + 0.5F) * p_228883_3_;
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.SOUTH);
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.NORTH);
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.EAST);
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.WEST);
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.DOWN);
-        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, p_228883_2_, p_228883_2_, 1.0F, 1.0F, 0.0F, 0.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.UP);
+        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, uZ(1.0F), uZ(0.0F), uZ(1.0F), uZ(0.0F), uZ(0.0F), uZ(0.0F), uZ(0.0F), uZ(0.0F), lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.SOUTH);
+        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, uZ(1.0F), uZ(0.0F), uZ(0.0F), uZ(1.0F), uZ(1.0F), uZ(1.0F), uZ(1.0F), uZ(1.0F), lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.NORTH);
+        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, uZ(0.0F), uZ(0.0F), uZ(0.0F), uZ(1.0F), uZ(1.0F), uZ(0.0F), uZ(0.0F), uZ(1.0F), lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.EAST);
+        this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, uZ(1.0F), uZ(1.0F), uZ(1.0F), uZ(0.0F), uZ(1.0F), uZ(0.0F), uZ(0.0F), uZ(1.0F), lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.WEST);
+        //this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.DOWN);
+        //this.renderFace(p_228883_1_, p_228883_4_, p_228883_5_, 0.0F, 1.0F, p_228883_2_, p_228883_2_, 1.0F, 1.0F, 0.0F, 0.0F, lvt_6_1_, lvt_7_1_, lvt_8_1_, Direction.UP);
     }
 
-    private void renderFace(T p_228884_1_, Matrix4f p_228884_2_, IVertexBuilder p_228884_3_, float p_228884_4_, float p_228884_5_, float p_228884_6_, float p_228884_7_, float p_228884_8_, float p_228884_9_, float p_228884_10_, float p_228884_11_, float p_228884_12_, float p_228884_13_, float p_228884_14_, Direction p_228884_15_) {
+    private void renderFace(T vacuumTileEntity, Matrix4f matrix, IVertexBuilder vertexBuilder, float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3, float p_228884_12_, float p_228884_13_, float p_228884_14_, Direction p_228884_15_) {
         if (true/*p_228884_1_.should*/||/*RenderFace(p_228884_15_)*/true) {
-            p_228884_3_.pos(p_228884_2_, p_228884_4_, p_228884_6_, p_228884_8_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
-            p_228884_3_.pos(p_228884_2_, p_228884_5_, p_228884_6_, p_228884_9_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
-            p_228884_3_.pos(p_228884_2_, p_228884_5_, p_228884_7_, p_228884_10_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
-            p_228884_3_.pos(p_228884_2_, p_228884_4_, p_228884_7_, p_228884_11_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
+            vertexBuilder.pos(matrix, x0, y0, z0).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
+            vertexBuilder.pos(matrix, x1, y0, z1).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
+            vertexBuilder.pos(matrix, x1, y1, z2).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
+            vertexBuilder.pos(matrix, x0, y1, z3).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
         }
 
+    }
+
+    // Unique Z axis
+    private float uZ(float z){
+        if (z==0F) return 0.0001F; else return 0.99981F;
     }
 
     protected int getPasses(double p_191286_1_) {
