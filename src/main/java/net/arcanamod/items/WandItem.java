90ff1e78d6d0ea3fb73aadf94aa0898c73f475f5
DcZipPL <dczip89@gmail.com>
2020-11-10 18:12:44 +0100

    Fix errors again

--- src/main/java/net/arcanamod/items/WandItem.java
+++ src/main/java/net/arcanamod/items/WandItem.java
@@ -137,13 +137,12 @@ public class WandItem extends Item{
 				// oh my god this code is terrible // YES, I know Xd.
 				// time for more VisUtils I guess
 				if(spell.getSpellCosts().toList().stream().allMatch(stack -> handler.findAspectInHolders(stack.getAspect()).getCurrentVis() >= stack.getAmount())){
-					ActionResultType result;
 					if (player.isCrouching())
-						result = spell.use(player, player.getHeldItem(hand), ICast.Action.SPECIAL);
+						Spell.runSpell(createBasicSpell(),player,player.getHeldItem(hand), ICast.Action.SPECIAL);
+						//result = spell.use(player, player.getHeldItem(hand), ICast.Action.SPECIAL);
 					else
-						result = spell.use(player, player.getHeldItem(hand), ICast.Action.USE);
+						Spell.runSpell(createBasicSpell(),player,player.getHeldItem(hand), ICast.Action.USE);
 					// remove aspects from wand if spell successes.
-					if (result != ActionResultType.FAIL && result != ActionResultType.PASS)
 					for(AspectStack cost : spell.getSpellCosts().toList())
 						handler.findAspectInHolders(cost.getAspect()).drain(cost, false);
 				}
