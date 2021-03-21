612f405fa14e01ca7112baeea2beaeb5fe88e7cd
Luna <izzeldeen03@gmail.com>
2021-03-21 18:22:21 +0000

    Fix effects: attribute modifiers no longer break when reopening worlds, taint doesn't kill stupidly fast, frozen reduces speed normally, life infused regens, warding increases armour by a sensible amount

--- src/main/java/net/arcanamod/effects/WardingEffect.java
+++ src/main/java/net/arcanamod/effects/WardingEffect.java
@@ -3,11 +3,12 @@ package net.arcanamod.effects;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 
-public class WardingEffect extends Effect {
+public class WardingEffect extends Effect{
+	
 	public WardingEffect(){
 		super(EffectType.BENEFICIAL, 0x3255FF);
 	}
-
+	
 	@Override
 	public boolean isReady(int duration, int amplifier){
 		return true;
