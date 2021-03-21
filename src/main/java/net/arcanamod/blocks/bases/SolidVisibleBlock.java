d32efe7e500bda165be18278b18155fc62945e2e
Luna <izzeldeen03@gmail.com>
2021-03-21 19:48:23 +0000

    actually fix smokey glass

---
+++ src/main/java/net/arcanamod/blocks/bases/SolidVisibleBlock.java
@@ -0,0 +1,10 @@
+package net.arcanamod.blocks.bases;
+
+import net.minecraft.block.BreakableBlock;
+
+public class SolidVisibleBlock extends BreakableBlock{
+	
+	public SolidVisibleBlock(Properties properties){
+		super(properties);
+	}
+}
